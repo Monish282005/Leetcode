b83d65764ba81e296c4a48f982acd45fab3bfbac
Monish282005 <monishmuthusamy28@gmail.com>
2026-02-18 12:00:20 +0530

    Time: 160 ms (7.96%), Space: 71.8 MB (7.31%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,12 +1,14 @@
 class Solution {
     public int countPrimes(int n) {
         int prime[] = new int[n + 1];
-
+    
         for (int i = 2; i * i <= n; i++) {
+            if(prime[i] != 1){
             for (int j = i * i; j <= n; j = j + i) {
                 if (j % i == 0 && prime[j] != 1) {
                     prime[j] = 1;
                 }
+            }
             }
         }
 
