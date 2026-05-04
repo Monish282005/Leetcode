e2d984b3e14872045fdbd2701a53d23feb0cb2a4
Monish282005 <monishmuthusamy28@gmail.com>
2026-05-04 14:46:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,20 +1,7 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) return false;
 
-        if(s.equals(goal))
-            return true;
-
-        StringBuilder sb = new StringBuilder(s);
-
-        for(int i = 0; i < s.length(); i++){
-            char c = sb.charAt(0);
-            sb.deleteCharAt(0);
-            sb.append(c);
-
-            if(sb.toString().equals(goal))
-                return true;
-        }
-
-        return false;
+        return (s + s).contains(goal);
     }
 }
