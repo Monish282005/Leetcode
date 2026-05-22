618a8c8feb6321460e647f63d9eccba8ad0d3ef3
Monish282005 <monishmuthusamy28@gmail.com>
2026-05-22 07:45:55 +0530

    Time: 1111 ms (71.31%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,8 @@
+
+select p.product_id, p.product_name
+from Product p
+join Sales s
+on p.product_id = s.product_id
+group by s.product_id
+having max(sale_date) between '2019-01-01' and '2019-03-31'
+and min(sale_date) between '2019-01-01' and '2019-03-31'
