3ec8f3758bd277796ae3725a3784b9fdd808ab1e
Monish282005 <monishmuthusamy28@gmail.com>
2026-05-27 12:05:23 +0530

    Time: 0 ms (100%), Space: 43.1 MB (32.94%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,15 +1,5 @@
 class Solution {
     public String toLowerCase(String s) {
-        StringBuilder sb = new StringBuilder();
-
-        for(char c: s.toCharArray()){
-            int val = c;
-
-            if(val >= 65 && val <= 90){
-                sb.append((char) (c + 32));
-            }else sb.append(c);
-        }
-
-        return sb.toString();
+        return s.toLowerCase();
     }
 }
