e176c2d86f2de09564b1e293bb7fa837624c4f46
Monish282005 <monishmuthusamy28@gmail.com>
2026-06-06 07:42:56 +0530

    Time: 2 ms (98.84%), Space: 46.6 MB (48%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -8,9 +8,9 @@ class Solution {
         for (int i = 1; i < n; i++)
             left[i] = left[i - 1] + nums[i - 1];
 
-        for(int i = n - 2; i >= 0; i--){
-            right[i] = right[i + 1] + nums[i + 1];
-        }
+       for(int i = 1; i < n; i++){
+        right[n - i - 1] = right[n - i] + nums[n - i];
+       }
 
         int res[] = new int[n];
 
