098e81afa4ff7093673a27325265e5aaae8a2741
Monish282005 <monishmuthusamy28@gmail.com>
2026-06-12 22:05:03 +0530

    Time: 8 ms (10.9%), Space: 53.8 MB (18.82%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -2,37 +2,31 @@ class Solution {
     public int findJudge(int n, int[][] trust) {
         List<List<Integer>> adj = new ArrayList<>();
 
-        for(int i = 0; i <= n; i++){
+        for (int i = 0; i <= n; i++)
             adj.add(new ArrayList<>());
-        }
 
-        for(int[] it: trust){
-            int u = it[0];
-            int v = it[1];
-            adj.get(u).add(v);
-        }
-        
+        for (int[] it : trust)
+            adj.get(it[0]).add(it[1]);
+
         int judge = -1;
-        for(int i = 1; i <= n; i++){
-            if(adj.get(i).size() == 0)
+        for (int i = 1; i <= n; i++)
+            if (adj.get(i).size() == 0)
                 judge = i;
-        }
 
-        if(judge == -1)
+        if (judge == -1)
             return -1;
 
         int count = 0;
 
-        for(int i = 1; i <= n; i++){
-            for(int nei: adj.get(i)){
-                if(nei == judge)
+        for (int i = 1; i <= n; i++)
+            for (int nei : adj.get(i)) {
+                if (nei == judge)
                     count++;
             }
-        }
 
-        if(count == n - 1)
+        if (count == n - 1)
             return judge;
-        
+
         return -1;
     }
 }
